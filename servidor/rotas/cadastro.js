6d8583427a86417ef20ffe6dd6f63e6305c0e792
Vitor Souza <vitorugo_kta@hotmail.com>
2022-11-25 16:03:29 -0300

    criando metodo posto do cadastros

--- servidor/rotas/cadastro.js
+++ servidor/rotas/cadastro.js
@@ -1,9 +1,23 @@
 const express = require('express');  // Importando pacote/módulo express
-const exphbs = require('express-handlebars');  // Importando pacote/módulo Express-Handlebars
+// const exphbs = require('express-handlebars');  // Importando pacote/módulo Express-Handlebars
 const app = express();      // Instânciando métodos do express na constante 
+
 
 const cadastros = app.get('/cadastros', (req,res)=>{
     res.render('cadastros', {layout:false});
 });
 
 module.exports = cadastros;
+
+const save = (app.post('cadastros/save', async (req, res) => {
+    const nome = req.body.firstname;
+    const sobrenome = req.body.lastname;
+    const setor = req.body.setor;
+    const numero = req.body.number;
+    const senha = req.body.password;
+    
+
+    
+
+
+}))
