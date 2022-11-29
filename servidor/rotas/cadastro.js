09e03662d3a35806934ed46e20caec1d4f93c5bb
Vitor Souza <vitorugo_kta@hotmail.com>
2022-11-29 10:38:48 -0300

    corrigido bug que a data estava vazia

--- servidor/rotas/cadastro.js
+++ servidor/rotas/cadastro.js
@@ -16,10 +16,10 @@ const cadastrosSave = app.post('/cadastros/save', async (req, res) => {
     const sobrenome = req.body.lastname;
     const cpf = req.body.cpf;
     const setor = req.body.setor;
-    const contato = req.body.contato;
-    const dataNascimento = req.body.dataNascimento;
-    const logadouro = req.body.logadouro;
-    const numero = req.body.numero;
+    const contato = req.body.number;
+    const data_nascimento = req.body.dataNascimento;
+    const logadouro = req.body.logad;
+    const numero = req.body.num;
     const bairro = req.body.bairro;
     const cidade = req.body.cidade;
     const cep = req.body.cep;
@@ -27,11 +27,14 @@ const cadastrosSave = app.post('/cadastros/save', async (req, res) => {
     const email = req.body.email;
     const senha = req.body.password;
 
-    await Associados.create({nome, sobrenome, cpf, setor, contato, dataNascimento, logadouro, numero,
+    console.log(data_nascimento)
+    
+    await Associados.create({nome, sobrenome, cpf, setor, contato, data_nascimento, logadouro, numero,
                             bairro, cidade, cep, estado, email, senha});
     
     res.redirect('/')
     console.log('Cadastrados')
+    
 });
 
 module.exports = {cadastros, cadastrosSave};
