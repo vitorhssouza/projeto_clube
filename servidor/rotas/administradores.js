3a17648c0e7fe9ddb2d637bb05db94eb056aff12
Vitor Souza <vitorugo_kta@hotmail.com>
2022-11-29 18:47:34 -0300

    Criando arquivo administração para criação de rota

---
+++ servidor/rotas/administradores.js
@@ -0,0 +1,3 @@
+const express = require('express');         // Importando pacote/módulo express
+const Associados = require('../banco_dados/Associados');
+const app = express();      // Instânciando métodos do express na constante 
