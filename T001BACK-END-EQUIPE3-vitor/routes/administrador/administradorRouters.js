e6e0d8ec2ddc9d529e11bce0b7f93f103ff2dd24
EricaRPV <ericagomesfernandes15@gmail.com>
2022-12-04 12:32:53 -0300

    altercao front

---
+++ T001BACK-END-EQUIPE3-vitor/routes/administrador/administradorRouters.js
@@ -0,0 +1,21 @@
+const { Router } = require('express');
+const express = require('express');
+const AdministradorController = require('../../controllers/AdministradorController')
+const router = express.Router();
+
+// rota principal da pagina admin
+router.get('/clube/admin', AdministradorController.associados);
+
+// Rota de exibir detalhes de associados
+router.get('/clube/admin/associado/detalhes/:id', AdministradorController.detalhes)
+
+// Rota de excluir associados
+router.get('/clube/admin/associado/delete/:id', AdministradorController.excluir)
+
+// Rota de Ativar status do associado
+router.get('/clube/admin/associado/situacao/ativar/:id', AdministradorController.ativar)
+
+// Rota de desativar status do associado
+router.get('/clube/admin/associado/situacao/desativa/:id', AdministradorController.desativa)
+module.exports = router;
+
